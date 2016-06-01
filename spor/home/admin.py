b3b3076038a575b9018a1684e11b789e6fba3e88
Jason Bristol <gethsemane369@gmail.com>
2016-06-01 13:48:08 -0400

    Fixed social contact dependencies

--- spor/home/admin.py
+++ spor/home/admin.py
@@ -1,39 +1,40 @@
 from django.contrib import admin
 from .models import TeamMember, Partner, Contact, Banner, Alert, Modal
-# from django import forms
-# from django.core.validators import EMPTY_VALUES
+from django.core.validators import EMPTY_VALUES
+from django import forms
 
-# class TeamMemberForm(forms.ModelForm):
-#     class Meta:
-#         model = TeamMember
+class TeamMemberForm(forms.ModelForm):
+    class Meta:
+        model = TeamMember
+        fields = '__all__'
 
-#     def clean(self):
-#         facebook = self.cleaned_data.get('facebook', False)
-#         linkedin = self.cleaned_data.get('linkedin', False)
-#         twitter = self.cleaned_data.get('twitter', False)
+    def clean(self):
+        facebook = self.cleaned_data.get('facebook', False)
+        linkedin = self.cleaned_data.get('linkedin', False)
+        twitter = self.cleaned_data.get('twitter', False)
 
-#         if facebook:
-#             facebook_url = self.cleaned_data.get('facebook_url', None)
-#             if facebook_url in EMPTY_VALUES:
-#                 self._errors['facebook_url'] = self.error_class([
-#                     'A URL is required is this team member has a Facebook page'])
+        if facebook:
+            facebook_url = self.cleaned_data.get('facebook_url', None)
+            if facebook_url in EMPTY_VALUES:
+                self._errors['facebook_url'] = self.error_class([
+                    'A URL is required if this team member has a Facebook page'])
 
-#         if linkedin:
-#             linkedin_url = self.cleaned_data.get('linkedin_url', None)
-#             if linkedin_url in EMPTY_VALUES:
-#                 self._errors['linkedin_url'] = self.error_class([
-#                     'A URL is required is this team member has a Linkedin page'])
+        if linkedin:
+            linkedin_url = self.cleaned_data.get('linkedin_url', None)
+            if linkedin_url in EMPTY_VALUES:
+                self._errors['linkedin_url'] = self.error_class([
+                    'A URL is required if this team member has a Linkedin page'])
 
-#         if twitter:
-#             twitter_url = self.cleaned_data.get('twitter_url', None)
-#             if twitter_url in EMPTY_VALUES:
-#                 self._errors['twitter_url'] = self.error_class([
-#                     'A URL is required is this team member has a Twitter page'])
+        if twitter:
+            twitter_url = self.cleaned_data.get('twitter_url', None)
+            if twitter_url in EMPTY_VALUES:
+                self._errors['twitter_url'] = self.error_class([
+                    'A URL is required if this team member has a Twitter page'])
 
-#         return self.cleaned_data
+        return self.cleaned_data
 
 class TeamMemberAdmin(admin.ModelAdmin):
-	pass
+	form = TeamMemberForm
 
 admin.site.register(TeamMember, TeamMemberAdmin)
 
@@ -42,8 +43,45 @@ class PartnerAdmin(admin.ModelAdmin):
 
 admin.site.register(Partner, PartnerAdmin)
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+    def clean(self):
+        facebook = self.cleaned_data.get('facebook', False)
+        linkedin = self.cleaned_data.get('linkedin', False)
+        twitter = self.cleaned_data.get('twitter', False)
+        gplus = self.cleaned_data.get('gplus', False)
+
+        if facebook:
+            facebook_url = self.cleaned_data.get('facebook_url', None)
+            if facebook_url in EMPTY_VALUES:
+                self._errors['facebook_url'] = self.error_class([
+                    'A URL is required if this contact has a Facebook page'])
+
+        if linkedin:
+            linkedin_url = self.cleaned_data.get('linkedin_url', None)
+            if linkedin_url in EMPTY_VALUES:
+                self._errors['linkedin_url'] = self.error_class([
+                    'A URL is required if this contact has a Linkedin page'])
+
+        if twitter:
+            twitter_url = self.cleaned_data.get('twitter_url', None)
+            if twitter_url in EMPTY_VALUES:
+                self._errors['twitter_url'] = self.error_class([
+                    'A URL is required if this contact has a Twitter page'])
+
+        if gplus:
+            gplus_url = self.cleaned_data.get('gplus_url', None)
+            if gplus_url in EMPTY_VALUES:
+                self._errors['gplus_url'] = self.error_class([
+                    'A URL is required if this contact has a Google+ page'])
+
+        return self.cleaned_data
+
 class ContactAdmin(admin.ModelAdmin):
-	pass
+	form = ContactForm
 
 admin.site.register(Contact, ContactAdmin)
 
